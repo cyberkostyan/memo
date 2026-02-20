1a629553eac2c94a1afc7d0abb8b9825bea0f1a3
kfromlarkit <kk@larkit.org>
2026-02-20 11:03:11 +0200

    Register PushModule, RemindersModule, and ScheduleModule

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { EventsModule } from "./events/events.module";
 import { UsersModule } from "./users/users.module";
+import { PushModule } from "./push/push.module";
+import { RemindersModule } from "./reminders/reminders.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, EventsModule, UsersModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    PrismaModule,
+    AuthModule,
+    EventsModule,
+    UsersModule,
+    PushModule,
+    RemindersModule,
+  ],
 })
 export class AppModule {}
